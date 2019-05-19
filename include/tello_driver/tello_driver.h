d7feec9b36331c4626507b3e933bd0132f6dd342
MasayaKataoka <ms.kataoka@gmail.com>
2019-05-19 17:13:48 +0900

    add URDF

--- include/tello_driver/tello_driver.h
+++ include/tello_driver/tello_driver.h
@@ -3,9 +3,10 @@
 
 // Headers in ROS
 #include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/PointStamped.h>
 
 // Heades in this package
-#include <tello_driver/TakeOff.h>
 #include <tello_driver/tello_command_builder.h>
 #include <tello_driver/udp_client.h>
 #include <tello_driver/udp_server.h>
@@ -18,12 +19,15 @@ namespace tello_driver
         TelloDriver(ros::NodeHandle nh,ros::NodeHandle pnh);
         ~TelloDriver();
     private:
-        bool TakeOffService(tello_driver::TakeOff::Request &req,tello_driver::TakeOff::Response &res);
-        const ros::NodeHandle nh_;
-        const ros::NodeHandle pnh_;
+        ros::NodeHandle nh_;
+        ros::NodeHandle pnh_;
         TelloCommandBuilder builder_;
         UdpClient cmd_client_;
         UdpServer res_server_;
+        ros::Subscriber takeoff_sub_;
+        void takeOffCallback(const std_msgs::Empty::ConstPtr msg);
+        ros::Subscriber move_sub_;
+        void moveCallback(const geometry_msgs::PointStamped::ConstPtr msg);
     };
 }
 
--- src/tello_driver.cpp
+++ src/tello_driver.cpp
@@ -4,9 +4,12 @@
 namespace tello_driver
 {
     TelloDriver::TelloDriver(ros::NodeHandle nh,ros::NodeHandle pnh)
-        : nh_(nh),pnh_(pnh),res_server_(tello_driver::TELLO_PORT_CMD)
+        : res_server_(tello_driver::TELLO_PORT_CMD)
     {
+        nh_ = nh;
+        pnh_ = pnh;
         cmd_client_.send(tello_driver::TELLO_IP,tello_driver::TELLO_PORT_CMD,builder_.buildInitCommand());
+        takeoff_sub_ = pnh_.subscribe("takeoff",1,&TelloDriver::takeOffCallback,this);
     }
 
     TelloDriver::~TelloDriver()
@@ -14,33 +17,9 @@ namespace tello_driver
 
     }
 
-    bool TelloDriver::TakeOffService(tello_driver::TakeOff::Request &req,tello_driver::TakeOff::Response &res)
+    void TelloDriver::takeOffCallback(const std_msgs::Empty::ConstPtr msg)
     {
-        ros::Time request_time = ros::Time::now();
-        ros::Duration duration = ros::Duration(0.1);
         cmd_client_.send(tello_driver::TELLO_IP,tello_driver::TELLO_PORT_CMD,builder_.buildTakeOffCommand());
-        while(true)
-        {
-            boost::circular_buffer<std::pair<ros::Time,std::string> > queue = res_server_.getMessageQueue();
-            if(queue.size() != 0 && queue[-1].first > request_time)
-            {
-                if(queue[-1].second == u8"ok")
-                {
-                    res.result = res.OK;
-                    return true;
-                }
-                if(queue[-1].second == u8"error")
-                {
-                    res.result = res.ERROR;
-                    return false;
-                }
-            }
-            if(ros::Time::now()-request_time>ros::Duration(tello_driver::TIMEOUT))
-            {
-                res.result = res.TIMEOUT;
-                return false;
-            }
-            duration.sleep();
-        }
+        return;
     }
 }
