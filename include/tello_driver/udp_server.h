560965fb74dae6c7b53bd4ccce483c38b2fa92ef
MasayaKataoka <ms.kataoka@gmail.com>
2019-05-19 14:52:48 +0900

    add udp_server class

--- include/tello_driver/udp_server.h
+++ include/tello_driver/udp_server.h
@@ -1,4 +1,28 @@
 #ifndef TELLO_DRIVER_UDP_SERVER_H_INCLUDED
 #define TELLO_DRIVER_UDP_SERVER_H_INCLUDED
 
+// Headers in this package
+#include <tello_driver/constants.h>
+
+// Headers in Boost
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+namespace tello_driver
+{
+    class UdpServer
+    {
+    public:
+        UdpServer(int port);
+        ~UdpServer();
+    private:
+        boost::asio::io_service io_service_;
+        boost::asio::ip::udp::socket socket_;
+        boost::asio::ip::udp::endpoint remote_endpoint_;
+        void receive(const boost::system::error_code&, std::size_t len);
+        boost::array<char, 512> recieve_buf_;
+    };
+}
+
 #endif  //TELLO_DRIVER_UDP_SERVER_H_INCLUDED
--- src/udp_server.cpp
+++ src/udp_server.cpp
@@ -1,1 +1,27 @@
 #include <tello_driver/udp_server.h>
+
+namespace tello_driver
+{
+    UdpServer::UdpServer(int port)
+        :socket_(io_service_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port))
+    {
+        socket_.async_receive_from(
+            boost::asio::buffer(recieve_buf_), remote_endpoint_,
+            boost::bind(&UdpServer::receive, this,
+            boost::asio::placeholders::error, _2));
+    }
+
+    UdpServer::~UdpServer()
+    {
+
+    }
+
+    void UdpServer::receive(const boost::system::error_code& error, size_t len)
+    {
+        if (!error || error == boost::asio::error::message_size)
+        {
+
+        }
+        return;
+    }
+}
